e57fb5a2673133d1f52341b781eddacd912f9c5d
Edu Aguilar <eduardoaguilartirado@gmail.com>
2026-04-20 22:08:47 +0200

    exercise

--- index-module.js
+++ index-module.js
@@ -7,3 +7,13 @@ console.log(a);
 
 export const b = 3;
 
+
+// Primer Ejercicio
+// escribamos código JS para que modifiquemos el valor del input y pongamos nuestro nombre.
+// pasados 5 segundos (setTimeout -> https://developer.mozilla.org/es/docs/Web/API/Element/setAttribute)
+// actualizaremos el input para que pase a ser de tipo password en lugar de texto (setAttribute)
+
+
+
+// Segundo ejercicio
+// vamos a incluir una imagen aleatoria dentro de la primera sección.
