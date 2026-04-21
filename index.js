c2aa40b1125bf044d4b4c66a80f037ece83b4770
Edu Aguilar <eduardoaguilartirado@gmail.com>
2026-04-21 20:53:03 +0200

    show tweets solution

--- index.js
+++ index.js
@@ -24,3 +24,40 @@ const tweets = [{
    message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
    likes: 400,
  }];
+
+// 1- Crear un nodo de destino para mis tweets
+
+  const container = document.querySelector('.tweets-container');
+
+// 2- iterar sobre el array de tweets
+tweets.forEach((tweet) => {
+
+  // 3- por cada tweet, crear una estructura HTML con los datos del tweet
+  const newTweet = document.createElement('div');
+  newTweet.classList.add('tweet')
+
+  // const header = document.createElement('h4');
+  // header.textContent = `${tweet.handler} - ${tweet.createdAt}`;
+
+  // const p1 = document.createElement('p')
+  // p1.textContent = tweet.message
+
+  // const p2 = document.createElement('p')
+  // p2.textContent = tweet.likes
+
+  // newTweet.appendChild(header)
+  // newTweet.appendChild(p1)
+  // newTweet.appendChild(p2)
+
+  newTweet.innerHTML = `
+    <h4>${tweet.handler} - ${tweet.createdAt}</h4>
+    <p>${tweet.message}</p>
+    <p>${tweet.likes}</p>
+  `
+
+  container.appendChild(newTweet)
+
+})
+
+
+// 4- añadir cada nodo creado al DOM
