54c753e718769bb6475da467996bb64583139bde
Edu Aguilar <eduardoaguilartirado@gmail.com>
2026-04-21 19:42:58 +0200

    show tweets exercise

--- index-module-2.js
+++
@@ -1,7 +0,0 @@
-import { b } from './index-module.js'
-
-console.log("hello world desde index.js! module - 2");
-
-
-console.log(b);
-
--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-// Fragmento JS plano - NO MODULO
-// las variables tienen un ámbito global
-
-console.log("hello world desde index.js!");
-
-const a = 5;
+const tweets = [{
+   handler: '@usuario1',
+   createdAt: new Date().toISOString(),
+   message: 'Eligendi fugiat veniam a neque omnis doloribus sequi porro expedita ullam excepturi.',
+   likes: 40,
+ },
+ {
+   handler: '@usuario2',
+   createdAt: new Date().toISOString(),
+   message: 'Laborum ad animi officia dolore nisi necessitatibus a porro! Odit!',
+   likes: 3,
+   image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1n_mfMKBHqSNMvn1dP_hChhGkmGblbArRaw&s"
+ },
+ {
+   handler: '@usuario3',
+   createdAt: new Date().toISOString(),
+   message: 'Ayer estuve en le parque y me lo pasé genial',
+   likes: 34,
+   image: "https://cdn.aarp.net/content/dam/aarp/entertainment/television/2017/07/1140-world-cup-trophy-ball-trivia-esp.jpg"
+ },
+ {
+   handler: '@usuario4',
+   createdAt: new Date().toISOString(),
+   message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+   likes: 400,
+ }];
